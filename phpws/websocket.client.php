7da37312224818c4420005e995615a744dc769ff
Risto <risto@live.nl>
2013-10-01 18:37:36 +0200

    Another header generation bug fixed in the client. Thanks to IngwiePhoenix (issue #13)

--- phpws/websocket.client.php
+++ phpws/websocket.client.php
@@ -153,7 +153,7 @@ class WebSocket implements WebSocketObserver {
     protected function buildHeaderArray() {
         $this->handshakeChallenge = WebSocketFunctions::randHybiKey();
 
-        $this->headers = array("GET" => "{$this->url} HTTP/1.1", "Connection:" => "Upgrade", "Host:" => "{$this->host}", "Sec-WebSocket-Key:" => "{$this->handshakeChallenge}", "Origin:" => "{$this->origin}", "Sec-WebSocket-Version:" => 13, "Upgrade:" => "websocket");
+        $this->headers = array("GET" => "{$this->path} HTTP/1.1", "Connection:" => "Upgrade", "Host:" => "{$this->host}", "Sec-WebSocket-Key:" => "{$this->handshakeChallenge}", "Origin:" => "{$this->origin}", "Sec-WebSocket-Version:" => 13, "Upgrade:" => "websocket");
 
         return $this->headers;
     }

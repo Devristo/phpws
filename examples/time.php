a2d46769c55c64db0c10425b4833f8ef2a11454d
Chris <risto@live.nl>
2013-11-26 19:42:04 +0100

    Time example

---
+++ examples/time.php
@@ -0,0 +1,29 @@
+#!/php -q
+<?php
+require_once("../vendor/autoload.php");
+use Devristo\Phpws\Server\WebSocketServer;
+
+$loop = \React\EventLoop\Factory::create();
+
+// Create a logger which writes everything to the STDOUT
+$logger = new \Zend\Log\Logger();
+$writer = new Zend\Log\Writer\Stream("php://output");
+$logger->addWriter($writer);
+
+// Create a WebSocket server using SSL
+$server = new WebSocketServer("tcp://0.0.0.0:12345", $loop, $logger);
+
+$loop->addPeriodicTimer(0.5, function() use($server, $logger){
+    $time = new DateTime();
+    $string = $time->format("Y-m-d H:i:s");
+    $logger->notice("Broadcasting time to all clients: $string");
+    foreach($server->getConnections() as $client)
+        $client->sendString($string);
+});
+
+
+// Bind the server
+$server->bind();
+
+// Start the event loop
+$loop->run();
